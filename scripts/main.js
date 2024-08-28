d579a9afc2cd358a99e6abe1d77530f58509ebe7
carl-michael-hash <carl.m.o.fuentes@outlook.com>
2024-08-28 11:31:22 -0600

    learn JS control flow

---
+++ scripts/main.js
@@ -0,0 +1,47 @@
+console.log ("Welcome to Conrol Flow")
+
+
+
+let amIHappy = true;
+let iKnowIt = true;
+
+if (amIHappy ===true) {
+    console.log ("clap my hands");
+    console.log ("stomp my feet");
+
+}
+
+if (amIHappy === true && iKnowIt ===true) {
+    console.log("im happy and i know it");
+}
+
+let age = 18;
+let isAlive = true;
+
+if (isAlive = true) {
+    console.log ("I vote that Mark be fired immediately");
+} else {
+    console.log("Live in the blissful ignorance of youth");
+}
+
+
+if (age >= 18) {
+    console.log ("I vote that Mark be fired immediately");
+} else {
+    console.log("Live in the blissful ignorance of youth");
+}
+
+let hour = 19;
+let action = "code";
+
+if (hour < 12) {
+    action = "drink coffee and code";
+} else if (hour < 17) {
+    action = "be tired, cry, and code";
+} else if (hour < 20) {
+    action ="go for a walk and have peace";
+} else {
+    action = "cry a lot and don't stop coding";
+}
+
+console.log (action)
